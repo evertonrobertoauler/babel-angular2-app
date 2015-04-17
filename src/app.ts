7ebe5729dca9ba2f33e7b3a7220e46153e7a2288
Éverton R. Auler <evertonrobertoauler@gmail.com>
2015-04-16 23:00:25 -0300

    adding form example

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'babel-core/polyfill';
 
 import {Component, View, bootstrap} from 'angular2/angular2';
+import {FormBuilder, Validators, FormDirectives, ControlGroup} from 'angular2/forms';
+import {Injectable, Inject} from 'angular2/di';
 import {Greeter} from './services.ts';
 
 @Component({
@@ -16,4 +18,30 @@ class HelloApp {
   }
 }
 
+@Component({selector: 'form-app'})
+@View({
+  template: `
+  <div [control-group]="form">
+    Full Name: <input control="fullName"><br />
+    Username: <input control="username"><br />
+    Favorite Color:<input control="favColor"><br />
+  </div>
+  {{ form.controls.fullName.value }}
+  `,
+  directives: [FormDirectives]
+})
+class FormApp {
+  form:ControlGroup;
+
+  constructor() {
+    let builder = new FormBuilder();
+    this.form = builder.group({
+      fullName: ["", Validators.required],
+      username: ["", Validators.required],
+      favColor: [""]
+    });
+  }
+}
+
 bootstrap(HelloApp);
+bootstrap(FormApp);
