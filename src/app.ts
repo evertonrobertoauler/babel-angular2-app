a7ce998c34ab33dc09980e3e9c06e134f08bc6cc
Éverton R. Auler <evertonrobertoauler@gmail.com>
2015-04-29 20:37:00 -0300

    Merge branch 'master' of https://github.com/shuhei/babel-angular2-app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import 'babel-core/polyfill';
-import { assert } from 'rtts_assert/rtts_assert';
+import {assert} from 'rtts_assert/rtts_assert';
 
 import {bootstrap} from 'angular2/angular2';
+import {Router} from 'angular2/router';
+import {RootRouter} from 'angular2/src/router/router';
+import {Pipeline} from 'angular2/src/router/pipeline';
 import {HelloApp} from './hello.ts';
 import {FormApp} from './form.ts';
+import {bind} from 'angular2/di';
 
-bootstrap(HelloApp);
 bootstrap(FormApp);
+
+bootstrap(HelloApp, [
+  bind(Router).toValue(new RootRouter(new Pipeline()))
+]);
--- src/hello.ts
+++ src/hello.ts
@@ -1,15 +1,37 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View, Attribute} from 'angular2/angular2';
+import {Router, RouterOutlet} from 'angular2/router';
 import {Greeter} from './services.ts';
 
 @Component({
-  selector: 'hello-app',
+  selector: 'hello',
   injectables: [Greeter]
 })
-@View({template: `<p>{{message}}</p>`})
-export class HelloApp {
+@View({
+  template: `
+    <p>{{message}}</p>
+  `
+})
+class Hello {
   message:string;
 
-  constructor(greeter:Greeter) {
-    this.message = greeter.hello('Angular 2 app');
+  constructor(greeter: Greeter) {
+    this.message = greeter.hello('Angular 2 App');
+  }
+}
+
+@Component({
+  selector: 'hello-app'
+})
+@View({
+  directives: [Hello, RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `
+})
+export class HelloApp {
+  constructor(router:Router) {
+    router
+      .config('/hello', Hello)
+      .then((_) => router.navigate('/hello'))
   }
 }
